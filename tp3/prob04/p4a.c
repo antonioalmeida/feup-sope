9f8180c414ea66c365ffa6094a37f4a3354af61c
theantonioalmeida <tony-alm@hotmail.com>
2017-03-10 01:02:51 +0000

    [T3] prob0[1-4] complete

---
+++ tp3/prob04/p4a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void) {
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("Fork ERROR ");
+        exit(1);
+    }
+
+    if (pid == 0)
+        printf("World!\n");
+    else
+        printf("Hello ");
+
+    exit(0);
+}
