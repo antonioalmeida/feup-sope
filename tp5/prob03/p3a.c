f8bfca04dc0e791978f9aef7a7806729c9d8b214
theantonioalmeida <tony-alm@hotmail.com>
2017-03-27 21:52:39 +0100

    [T#5] Move last commit's file to proper location

--- tp5/prob03/p3a.c
+++ tp5/prob03/p3a.c
@@ -1,69 +1,32 @@
-#include
-<stdio.h>
-#include
-<stdlib.h>
-#define
-   MAXLINE   1000
-#define
-   PAGER
-"/bin/more"
-int
- main(
-int
- argc,
-char
- *argv[])
-{
-char
-   line[MAXLINE];
-      FILE   *fpin, *fpout;
-if
- (argc != 2) { printf(
-"usage: %s filename\n"
-,argv[0]); exit(1); }
-if
- ((fpin = fopen(argv[1],
-"r"
-)) == NULL)
-      {
-            fprintf(stderr,
-"can't open %s"
-, argv[1]); exit(1);
-      }
-if
- ((fpout = popen(PAGER,
-"w"
-)) == NULL)
-      {
-            fprintf(stderr,
-"popen error"
-); exit(1);
-      }
-/* copy filename contents to pager - file=argv[1] */
-while
- (fgets(line, MAXLINE, fpin) != NULL)
-      {
-if
- (fputs(line, fpout) == EOF)
-            {
-                  printf(
-"fputs error to pipe"
-); exit(1);
-            }
-      }
-if
- (ferror(fpin))
-      {
-            fprintf(stderr,
-"fgets error"
-); exit(1);
-      }
-if
- (pclose(fpout) == -1)
-      {
-            fprintf(stderr,
-"pclose error"
-);
-            exit(1);      }
-      exit(0);
-} 
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE 1000
+#define SORT "/usr/bin/sort" // macOS
+
+int main(int argc, char *argv[]) {
+  char line[MAXLINE];
+  FILE *fpin, *fpout;
+
+  if (argc != 2) {
+    printf("usage: %s filename\n", argv[0]);
+    exit(1);
+  }
+
+  // Opening file
+  if ((fpin = fopen(argv[1], "r")) == NULL) {
+    fprintf(stderr, "can't open %s", argv[1]);
+    exit(1);
+  }
+
+  // Opening sort "file" as pipe to write to
+  if ((fpout = popen(SORT, "w")) == NULL) {
+    fprintf(stderr, "popen error");
+    exit(1);
+  }
+
+  while (fgets(line, MAXLINE, fpin) != NULL) // Reading line from input file
+    fputs(line, fpout);                      // Writing line to sort
+
+  pclose(fpout);
+  exit(0);
+}
