68a7680887d9ca92b85f591799eaf8ec3882aba5
theantonioalmeida <tony-alm@hotmail.com>
2017-03-28 02:29:48 +0100

    [T#5] prob04 complete && beautify some files

--- tp5/prob03/p3a.c
+++ tp5/prob03/p3a.c
@@ -4,29 +4,29 @@
 #define SORT "/usr/bin/sort" // macOS
 
 int main(int argc, char *argv[]) {
-  char line[MAXLINE];
-  FILE *fpin, *fpout;
+        char line[MAXLINE];
+        FILE *fpin, *fpout;
 
-  if (argc != 2) {
-    printf("usage: %s filename\n", argv[0]);
-    exit(1);
-  }
+        if (argc != 2) {
+                printf("usage: %s filename\n", argv[0]);
+                exit(1);
+        }
 
-  // Opening file
-  if ((fpin = fopen(argv[1], "r")) == NULL) {
-    fprintf(stderr, "can't open %s", argv[1]);
-    exit(1);
-  }
+        // Opening file
+        if ((fpin = fopen(argv[1], "r")) == NULL) {
+                fprintf(stderr, "can't open %s", argv[1]);
+                exit(1);
+        }
 
-  // Opening sort "file" as pipe to write to
-  if ((fpout = popen(SORT, "w")) == NULL) {
-    fprintf(stderr, "popen error");
-    exit(1);
-  }
+        // Opening sort "file" as pipe to write to
+        if ((fpout = popen(SORT, "w")) == NULL) {
+                fprintf(stderr, "popen error");
+                exit(1);
+        }
 
-  while (fgets(line, MAXLINE, fpin) != NULL) // Reading line from input file
-    fputs(line, fpout);                      // Writing line to sort
+        while (fgets(line, MAXLINE, fpin) != NULL) // Reading line from input file
+                fputs(line, fpout);          // Writing line to sort
 
-  pclose(fpout);
-  exit(0);
+        pclose(fpout);
+        exit(0);
 }
--- tp5/prob03/p3b.c
+++ tp5/prob03/p3b.c
@@ -7,19 +7,19 @@
 // $ cat filename.txt | ./p3b
 
 int main(int argc, char *argv[]) {
-  char line[MAXLINE];
-  FILE *fpout;
+        char line[MAXLINE];
+        FILE *fpout;
 
-  // Opening sort "file" as pipe to write to
-  if ((fpout = popen(SORT, "w")) == NULL) {
-    fprintf(stderr, "popen error");
-    exit(1);
-  }
+        // Opening sort "file" as pipe to write to
+        if ((fpout = popen(SORT, "w")) == NULL) {
+                fprintf(stderr, "popen error");
+                exit(1);
+        }
 
-  while (fgets(line, MAXLINE, stdin) !=
-         NULL)          // Reading line from stdin (cat command's output)
-    fputs(line, fpout); // Writing line to sort
+        while (fgets(line, MAXLINE, stdin) !=
+               NULL)    // Reading line from stdin (cat command's output)
+                fputs(line, fpout); // Writing line to sort
 
-  pclose(fpout);
-  exit(0);
+        pclose(fpout);
+        exit(0);
 }
